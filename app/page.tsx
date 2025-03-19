657883fe59ccc43d953b0f6260560b64243908a5
Maryambghm2 <maryambghm2@outlook.fr>
2025-03-19 12:13:52 +0100

    Modification de la page login

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import LoginPage from "./front/components/LoginFunc";
+import Link from "next/link";
 
-export default function HomePage() {
-    return LoginPage
+export default function Home() {
+    return (
+        <p><Link href='front/login'>Bienvenue chez Agora community</Link></p>
+    )
 }
